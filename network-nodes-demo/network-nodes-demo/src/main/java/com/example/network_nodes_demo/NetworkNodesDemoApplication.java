81155d357d88bcf4d01a177d86a98f8ae2dd9a51
Zeljko Pozgaj <pozgajzeljko@gmail.com>
2026-05-19 05:07:08 +0200

    Zadatak1 init

---
+++ network-nodes-demo/network-nodes-demo/src/main/java/com/example/network_nodes_demo/NetworkNodesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.network_nodes_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkNodesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkNodesDemoApplication.class, args);
+	}
+
+}
